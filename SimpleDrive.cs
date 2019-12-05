3735361ead8ccab1cc3fea7b6a547f65660e6e35
Player 2 <tomarchioalfio5@gmail.com>
2019-12-05 15:33:49 +0100

    Uploaded the script

---
+++ SimpleDrive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDrive: MonoBehaviour
+{
+	//various variables
+	public float speed;
+	public float turn;
+	private float horizontalInput;
+	private float forwardInput;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Get input from horizontal and vertical axis
+		horizontalInput = Input.GetAxis("Horizontal");
+		forwardInput = Input.GetAxis("Vertical");
+		
+        //Drive and turn the car based from input gathered above
+		transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+		transform.Rotate(Vector3.up, turn * horizontalInput * Time.deltaTime);
+    }
+}
